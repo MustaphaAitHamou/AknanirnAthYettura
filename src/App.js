0cdd4ea35293bbd229c011476c75a3a52fc4a96b
Mustapha Ait Hamou <70661985+MustaphaAitHamou@users.noreply.github.com>
2022-12-22 22:44:41 +0100

    menu added

--- src/App.js
+++ src/App.js
@@ -20,16 +20,16 @@ function App() {
 
       <Header />
       <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/foods" element={<AllFoods />} />
-      <Route path="/foods/:id" element={<FoodDetails />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/contact" element={<Contact />} />
-            </Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/foods" element={<AllFoods />} />
+        <Route path="/foods/:id" element={<FoodDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
 
     </div>
