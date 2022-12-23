8bdbcca5fa4a668571297b7be1e737bc7ce563b8
Mustapha Ait Hamou <70661985+MustaphaAitHamou@users.noreply.github.com>
2022-12-23 02:16:26 +0100

    footer almost done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ import logo from '../../assets/images/res-logo.png';
 
 import '../../styles/footer.css';
 
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return <footer className="footer">
     <Container>
@@ -18,7 +20,7 @@ const Footer = () => {
         </Col>
 
         <Col lg='3' md='4' sm='6'>
-          <h5>Temps de livraison</h5>
+          <h5 className='footer__title'>Temps de livraison</h5>
           <ListGroup className='deliver__time-list'>
             <ListGroupItem className=' delivery__time-item border-0 ps-0'>
               <span>De Vendredi à Dimanche</span>
@@ -30,11 +32,63 @@ const Footer = () => {
               <p>20h à 23h</p>
             </ListGroupItem>
           </ListGroup>
+
         </Col>
-        <Col lg='3' md='4' sm='6'></Col>
-        <Col lg='3' md='4' sm='6'></Col>
+
+        <Col lg='3' md='4' sm='6'>
+        <h5 className='footer__title'>Contact</h5>
+          <ListGroup className='deliver__time-list'>
+            
+            <ListGroupItem className=' delivery__time-item border-0 ps-0'>
+              <p>Localisation : Nice</p>
+            </ListGroupItem>
+            
+            <ListGroupItem className=' delivery__time-item border-0 ps-0'>
+              <span>Téléphone: 07-83-47-77-00</span>
+            </ListGroupItem>
+
+            <ListGroupItem className=' delivery__time-item border-0'>
+              <span>Email: example@gmail.com</span>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        
+        <Col lg='3' md='4' sm='6'>
+        <h5 className='footer__title'>Newsletter</h5>
+        <p>Abonnez-vous à notre newsletter !</p>
+        <div className='newsletter'>
+          <input type="email" placeholder='Entrez-votre email' />
+          <span>
+            <i className="ri-send-plane-line"></i>
+          </span>
+        </div>
+        </Col>
 
       </Row>
+
+      <Row>
+        <Col lg='6' md='6'>
+          <p>Copyright - 2022, Site web développé par Aknanir nAth Yettura. 
+            Tous droits reservés.</p>
+        </Col>
+
+        <Col lg='6' md='6'>
+        <div className='social__links'>
+          <p>Follow: </p>
+          <span> <Link to='www.facebook.com'>
+            <i className="ri-facebook-line"></i></Link> </span>
+
+            <span> <Link to='www.github.com'>
+            <i className="ri-github-line"></i></Link> </span>
+
+            <span> <Link to='www.youtube.com'>
+            <i className="ri-youtube-line"></i></Link> </span>
+
+        </div>
+        </Col>
+      </Row>
+
+
     </Container>
   </footer>
 }
