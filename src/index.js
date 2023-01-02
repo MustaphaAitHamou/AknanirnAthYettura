46ddf976bc00e460a4228c1f0884f1277af49037
Mustapha Ait Hamou <70661985+MustaphaAitHamou@users.noreply.github.com>
2023-01-02 19:21:26 +0100

    creating cart

--- src/index.js
+++ src/index.js
@@ -8,13 +8,18 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 
+import store from "./store/store";
+import { Provider } from "react-redux";
+
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+<React.StrictMode>
   <Router>
-  <App />
-</Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
 </React.StrictMode>,
   document.getElementById("root")
 );
