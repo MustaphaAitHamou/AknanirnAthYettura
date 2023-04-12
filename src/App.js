da7d0bd60e813d76751b8aa8f66e79c09b30028d
Mustapha Ait-Hamou <mustaphaithamou@gmail.com>
2023-04-12 14:31:16 +0200

    bug fixed

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import Header from "./components/Header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import { Routes, Route, Navigate } from "react-router-dom"
-import Home from "./pages/Home"
-import AllFoods from "./pages/AllFoods";
-import FoodDetails from "./pages/FoodDetails";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import Contact from "./pages/Contact";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+
 import React from "react";
+import Layout from "./components/Layout/Layout.js";
 
 
 function App() {
-  return (
-
-
-    <div className="App">
-
-      <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/foods" element={<AllFoods />} />
-        <Route path="/foods/:id" element={<FoodDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-
-    </div>
-
-  )
+  return <Layout/>
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -14,12 +14,12 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-<React.StrictMode>
-  <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </Router>
-</React.StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
   document.getElementById("root")
 );
