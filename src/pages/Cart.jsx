8513a8890e67edacbc7502aebe98e9e584c48dc1
Mustapha Ait-Hamou <mustaphaithamou@gmail.com>
2023-04-14 15:38:12 +0200

    Cart page builded

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
+import CommonSection from '../components/UI/common-section/CommonSection';
+import Helmet from '../components/Helmet/Helmet';
+import '../styles/cart-page.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { Container, Row, Col } from 'reactstrap';
+import { cartActions } from '../store/shopping-cart/cartSlice';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
 
-export default Cart
+  const cartItems = useSelector(state=> state.cart.cartItems)
+  const totalAmount = useSelector(state=> state.cart.totalAmount)
+
+  return (
+    <Helmet title='Cart'>
+      <CommonSection title='Votre carte'/>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12'>
+              { 
+                cartItems.length === 0 ? ( <h5 className='text-center'>Votre carte est vide</h5>
+                ) : ( <table className="table table-bordered">
+                  <table className='table table-bordered'>
+                    <thead>
+                      <tr>
+                        <th>Image</th>
+                        <th>Product Title</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Delete</th>
+                      </tr>
+                    </thead>
+
+                    <tbody>
+                      {
+                        cartItems.map((item)=> <Tr item={item} key={item.id}/>)
+                      }
+                      
+                    </tbody>
+                  </table>
+                </table>
+              )}
+
+              <div className='mt-4'>
+                <h6>Total : <span className='cart__subtotal'>{totalAmount}</span>€</h6>
+                <p>Taxes and shipping will calculate at the checkout</p>
+                <div className='cart__page-btn'>
+                  <button className="addTOCart__btn me-4"><Link to='/foods'>Continue shopping</Link></button>
+                  <button className="addTOCart__btn"><Link to='/checkout'>Procéder au paiement</Link></button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+const Tr = (props)=>{
+
+  const { id, image01, title, price, quantity } = props.item
+  const dispatch = useDispatch();
+  
+  const deleteItem = ()=>{
+    dispatch(cartActions.deleteItem(id))
+  }
+
+  return (
+    <tr>
+      <td className='text-center cart__img-box'>
+        <img src={image01} alt=""/>
+      </td>
+      <td className='text-center'>{title}</td>
+      <td className='text-center'>{price}</td>
+      <td className='text-center'>{quantity}</td>
+      <td className='text-center cart__item-del' onClick={deleteItem}>
+        <i className="ri-delete-bin-line"></i></td>
+    </tr>
+  );
+};
+
+export default Cart;
